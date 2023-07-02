39b4c8feff2afe2f828cec220327928d6419bbb8
Tom Holiday <tom.holiday@mac.com>
2023-07-02 13:48:11 -0600

    testing github function

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
       {/* <div className="background-image"></div> */}
         {/* <ParticlesBg type="circle" bg={true} /> */}
         <FrenchVocabularyGame/>
+        <div>I made a change</div>
         <div className="fixed-bottom-right">Made by Tom Holiday</div>
       </div>
       // style={{ backgroundImage: `url(${FrenchFlag})` }}
