3ecb416d65b5c48777e3a946c9424054193becd8
Tom Holiday <tom.holiday@mac.com>
2023-07-16 22:48:27 +0100

    returned to working programme, reayd to transfer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import React from 'react';
 import TextInput from './Components/TextInput/TextInput';
-import PopUpInformation from './Components/PopUpInformation/PopUpInformation';
 import pic from './Background/3.jpg';
 import ParticlesBg from "particles-bg";
 import FrenchVocabularyGame from './Components/FrenchVocabularyGame/FrenchVocabularyGame';
-
-
-const popupButton = document.getElementById("popupButton");
-const popupBox = document.getElementById("popupBox");
-const closeBtn = document.getElementsByClassName("close")[0];
 
 
 class App extends React.Component {
@@ -20,31 +14,12 @@ class App extends React.Component {
       {/* <div className="background-image"></div> */}
         {/* <ParticlesBg type="circle" bg={true} /> */}
         <FrenchVocabularyGame/>
-        {/* <PopUpInformation/> */}
-        <script>
-            <button id="popupButton">Click Me</button>
-            <div id="popupBox" class="popup">
-            <div class="popup-content">
-                <span class="close">&times;</span>
-                <h2>Pop-up Box Content</h2>
-                <p>This is the content of the pop-up box.</p>
-            </div>
-            </div>
-        </script>
         <div className="fixed-bottom-right">Made by Tom Holiday</div>
       </div>
       // style={{ backgroundImage: `url(${FrenchFlag})` }}
     );
   }
 }
-
-popupButton.addEventListener("click", function() {
-  popupBox.style.display = "block";
-});
-
-closeBtn.addEventListener("click", function() {
-  popupBox.style.display = "none";
-});
 
 // Best backgrounds: 3, 8, 9?, 10, 11?, 12?, 13?, 14, 16?
 
