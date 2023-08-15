40364564631c762387debcef07fc0062ea63b642
TomHolidayUK <tom.holiday@mac.com>
2023-08-15 17:53:56 +0100

    test8

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -36,7 +36,7 @@ class Register extends Component {
         })
             .then(response => response.json())
             .then(user => {
-                if (user) {
+                if (user.id) {
                     this.props.loadUser(user)
                     this.props.onRouteChange('setup');
                 }
