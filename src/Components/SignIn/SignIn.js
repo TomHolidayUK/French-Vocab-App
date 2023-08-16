1b19a78b2a502593b88952130ad3c79b2b309023
TomHolidayUK <tom.holiday@mac.com>
2023-08-16 11:15:52 +0100

    test14

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -42,7 +42,7 @@ class SignIn extends Component {
         // fetch('http://localhost:3000/signin', {
         //    fetch('https://nameless-savannah-12192-f4ca04c7a238.herokuapp.com/signin', {
             fetch('https://learn-french-vocabulary-api-5d216bdc9555.herokuapp.com/signin', {
-            // mode: 'no-cors',
+            mode: 'no-cors',
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
